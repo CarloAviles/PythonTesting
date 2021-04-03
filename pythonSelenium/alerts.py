a47d97f6c07258f09a73abf78aa00fb74a23fd97
Carlos <adrian.ag666@gmail.com>
2021-04-02 21:12:36 -0600

    terminado Alerts

--- pythonSelenium/alerts.py
+++ pythonSelenium/alerts.py
@@ -1,10 +1,20 @@
 #Este codigo es para hacer la prueba con Selenium el manipular un ventana de mensaje
+import time
 
 from selenium import webdriver
+
+validateText= "Option3"
 
 driver=webdriver.Chrome(executable_path="C:\\chromedriver.exe")
 driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/AutomationPractice")
 
-driver.find_element_by_css_selector("name").send_keys("Option3")
+driver.find_element_by_css_selector("#name").send_keys("Option3")
+
 driver.find_element_by_id("alertbtn").click()
+alert = driver.switch_to.alert
+alertText = alert.text
+assert validateText in alertText
+alert.accept()
+#Para dar en el boton de cancelar o negar
+#alert.dismiss()
---
+++ pythonSelenium/waitDemo.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="C:\\chromedriver.exe")
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+
